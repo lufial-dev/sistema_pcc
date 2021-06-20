ae4ec688892399d50a94c91baacf6e31d3a02ec1
filipealv <lfilipealvesdasilva@gmail.com>
2021-06-20 01:18:17 -0300

    backup

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -2,12 +2,26 @@ function showModalTermos(){
     document.getElementById("m-modal").style.display = "flex";
 }
 
+function showModalLogin(){
+    document.getElementById("m-modal-registro").style.display = "none";
+    document.getElementById("m-modal").style.display = "none";
+    document.getElementById("m-modal-login").style.display = "flex";
+}
+
+function showModalRegistro(){
+    document.getElementById("m-modal-login").style.display = "none";
+    document.getElementById("m-modal-registro").style.display = "flex";
+}
+
 function closeModalTermos(){
     document.getElementById("m-modal").style.display = "none";
+}
+
+function closeModalMenu(){
     document.getElementById("m-modal-menu").style.display = "none";
 }
 
+
 function showModalMenu(){
-    document.getElementById("m-modal").style.display = "none";
     document.getElementById("m-modal-menu").style.display = "flex";
 }
